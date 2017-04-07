64124abffa6f0b00db4d020050719e786860a42d
He1md4ll <martin.suckau@web.de>
2017-04-07 09:10:07 +0200

    Initial commit

---
+++ src/main/java/edu/hs/bremen/MicroApplication.java
@@ -0,0 +1,12 @@
+package edu.hs.bremen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroApplication.class, args);
+	}
+}
