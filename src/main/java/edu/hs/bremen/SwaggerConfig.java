ad238716c954c329773f3e7c5950a273b285dc42
Martin Suckau <martin.suckau@web.de>
2017-06-15 10:49:16 +0200

    fixed swagger config

--- src/main/java/edu/hs/bremen/SwaggerConfig.java
+++ src/main/java/edu/hs/bremen/SwaggerConfig.java
@@ -2,6 +2,7 @@ package edu.hs.bremen;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -15,9 +16,10 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.regex("/*"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
                 .build()
-                .useDefaultResponseMessages(Boolean.TRUE);
+                .useDefaultResponseMessages(Boolean.FALSE)
+                .securitySchemes(null);
     }
 }
